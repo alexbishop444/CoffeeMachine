38ac0b272b89aa36a9672de42483049aa9d10c3d
Alex Bishop <alex.bishop@myob.com>
2019-10-11 15:52:18 +1100

    got rid of char array and used .split instead

--- Tests/DrinkMakerTest.java
+++ Tests/DrinkMakerTest.java
@@ -22,4 +22,14 @@ public class DrinkMakerTest {
         String expected = "Drink maker makes 1 coffee with no sugar - and therefore no stick";
         Assert.assertEquals(expected,actual);
     }
+    @Test
+    public void makeCoffeeWith55Sugars() {
+        Order order = new Order(55,DrinkType.COFFEE);
+        OrderConverter converter = new OrderConverter();
+        String orderString = converter.convertOrder(order);
+        DrinkMaker drinkMaker = new DrinkMaker();
+        String actual = drinkMaker.makeDrink(orderString);
+        String expected = "Drink maker makes 1 coffee with 55 sugars and a stick";
+        Assert.assertEquals(expected,actual);
+    }
 }
--- src/DrinkMaker.java
+++ src/DrinkMaker.java
@@ -1,19 +1,22 @@
+import java.util.Arrays;
+
 public class DrinkMaker implements DrinkMakerInterface {
 
     public String makeDrink(String order) {
-        char[] orderAsCharArray = order.toCharArray();
-        char drinkFromOrder = orderAsCharArray[0];
-        char sugars = orderAsCharArray[2];
+        String[] test = order.split(":",5);
+//        System.out.println(Arrays.toString(test));
+        String drinkFromOrder = test[0];
+        String sugars = test[1];
         String drink = "";
         switch (drinkFromOrder) {
-            case 'T': drink = "tea";
+            case "T": drink = "tea";
             break;
-            case 'H': drink = "chocolate";
+            case "H": drink = "chocolate";
             break;
-            case 'C': drink = "coffee";
+            case "C": drink = "coffee";
             break;
         }
-        if(sugars == ':') {
+        if(sugars.equals(":")) {
             return "Drink maker makes 1 " + drink + " with no sugar - and therefore no stick";
         }
         return "Drink maker makes 1 " + drink +  " with " + sugars +  " sugars and a stick";
